f2de9a12cdfddec2a0065afcf425d3afa75875a6
caio_godinho <105987355+caio-godinho@users.noreply.github.com>
2023-09-28 09:46:11 -0300

    Update 1- Manipulação de dados em registradores e endereços de memória por meio de instruções de transferência de dados.asm

--- Atividade_1/1- Manipulação de dados em registradores e endereços de memória por meio de instruções de transferência de dados.asm
+++ Atividade_1/1- Manipulação de dados em registradores e endereços de memória por meio de instruções de transferência de dados.asm
@@ -1,48 +1,62 @@
 
+;Origem
 org 0000h
 
+;Inicia label da fun√ß√£o principal
 main:
+	;Move o valor 3 para o registrador acumulador
 	mov A, #03h ;1 us
 
+	;Move o valor 0 para o registrador acumulador
 	mov A, #00h ;1 us
 
+	;Move o valor 5 para o registrador R0
 	mov R0, #05h ;1 us
 
+	;Move o valor 7 para o registrador B
 	mov B, #07h ;2 us
 
+	;Move o valor da porta P1 para o endere√ßo 7C
 	mov 7Ch, P1 ;2 us
 
+	;Coloca o bit RS0 em 1, selecionando outro banco de registradores, e move o valor em 7C para o R0 
 	setb RS0 ;1 us
 	mov R0, 7Ch ;2 us
 
+	;move o valor em R0 para o endere√ßo 6F
 	mov 6Fh, R0 ;2 us
 
+	;Move o valor em 6F para o registrador R1
 	mov R1, #6Fh ;1 us
 
+	;Move o valor guardado no endere√ßo correspondente ao valor guardado em R1 para o registrador acumulador
 	mov A, @R1 ;1 us
 
-	mov DPTR, #0FFFFFFFh ;2 us
+	;Move o valor 9A5B para o registrador DPTR
+	mov DPTR, #9A5Bh ;2 us
 
+	;Espera 1us
 	NOP ;1 us
-	
+
+	;Fim do c√≥digo
 	end
 
-;(a) O tempo de cada instruÁ„o foi colocado ao lado de cada linha (em microssegundos).
+;(a) O tempo de cada instru√ß√£o foi colocado ao lado de cada linha (em microssegundos).
 ;O tempo total foi de 17 microssegundos
 
-;(b) Como a frequÍncia de clock do sistema È de 12 MHz, temos que existem 12 ciclos em 
+;(b) Como a frequ√™ncia de clock do sistema √© de 12 MHz, temos que existem 12 ciclos em 
 ;1 microssegundo. Portanto, programa existem 12 [ciclos] * 17 [us] = 154 microssegundos
 
 ;(c) Ao executar o comando, o registrador ACC recebeu o valor FF, que estava guardado na porta 1.
-;Quando o comando reset È acionado, os bits da porta 1 s‚o todos configurados como 1, indicando que
-;eles est‚o configurados como 
+;Quando o comando reset √© acionado, os bits da porta 1 s√¢o todos configurados como 1, indicando que
+;eles est√¢o configurados como 
 ;input. 
 
-;(d) O acumulador recebeu o valor guardado no endereÁo 6F, que È o valor guardado em R1.
+;(d) O acumulador recebeu o valor guardado no endere√ßo 6F, que √© o valor guardado em R1.
 
-;(e) Ao mover o valor para o registrados, percebeu-se uma mudanÁa nos registradores DPH e DPL.
-;… possÌvel mover um valor de 4 dÌgitos para DPTR pois ele È formado por 4 registradores: DP0l, 
-;DP0H, DP1L, DP1H. O valor m·ximo em hexadecimal que se pode guardar em DPTR È  FFFFFFF.
+;(e) Ao mover o valor para o registrados, percebeu-se uma mudan√ßa nos registradores DPH e DPL.
+;√â poss√≠vel mover um valor de 4 d√≠gitos para DPTR pois ele √© formado por 4 registradores: DP0l, 
+;DP0H, DP1L, DP1H. O valor m√°ximo em hexadecimal que se pode guardar em DPTR √©  FFFFFFF.
 
 
 	
